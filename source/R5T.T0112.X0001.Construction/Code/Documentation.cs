be285426697f0dd30ebc7ecab57d25a846c4a6e8
davidcoats <david@getsafetycone.com>
2021-12-21 11:28:16 -0800

    Initial commit of source-stub.

---
+++ source/R5T.T0112.X0001.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0112.X0001.Construction
+{
+    /// <summary>
+    /// Construction project for Public, common, Rivet-style (but not specific) ISolutionName and related extensions..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.T0112.X0001/Code/Classes/Class1.cs
@@ -0,0 +1,9 @@
+using System;
+
+
+namespace R5T.T0112.X0001
+{
+    public class Class1
+    {
+    }
+}
